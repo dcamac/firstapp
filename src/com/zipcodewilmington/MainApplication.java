9273ce2c79e467e6151b1d01a5914e8d1a1376d4
Dylan Camac <dylancamac@zipcoders-MacBook-Pro-5.local>
2018-02-06 16:02:15 -0500

    first commit

---
+++ src/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,9 @@
+package com.zipcodewilmington;
+
+public class MainApplication {
+// main method that runs your application
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
